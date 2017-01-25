5a1b5ad8fc08a5ed238633bd563ce9d3c29a08cf
RobinROAR <zrb915@live.com>
2017-01-24 20:07:11 -0600

    01242017

--- tutorial_1.py
+++ tutorial_1.py
@@ -1,68 +1,79 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
+#通过学习理解官方教程，一方面掌握TF的基本用法，另一方面学习先进的Python代码风格和规范
 #Robin  2016.6
-#Tensorflow 官方文档 教程1  MNIST机器学习入门
-
+# =============================================================
+"""
+learning by Tensorflow Tutorial: mnist_softmax
+"""
+#引用
+#版本兼容
+from __future__ import division
+from __future__ import print_function
+#解析命令行参数
+import argparse
+import sys
+#TF
 import tensorflow as tf
-#导入mnist数据集
 import tensorflow.examples.tutorials.mnist.input_data as input_data
 
-#####~~~~导入数据
-'''
-mnist_train: 55000
-mnist.train.images : [60000,784]
-& mnist.train.labels  : [60000,10]
-mnist_test:  10000, 10
-'''
-mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
+#通过设置flags，将dir参数包装到代码内部，只用argparse处理，不传参。
+FLAGS = None
 
-###~~~~~构建模型
-#创建一个交互单元，x为占位符，None表示张量第一维可以是任何长度
-x = tf.placeholder(tf.float32,[None,784])
-#用Variable更好的代表参数
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([1,10]))
+#主函数
+def main(_):
+    #导入数据,类型为tf定义的dataset，以数据名命名
+    #FLAGS通过argparse定义并增加参数。
+    mnist  = input_data.read_data_sets(FLAGS.dir, one_hot=True)
 
-# W1 = tf.Variable(tf.zeros([30,10]))
-# b1 = tf.Variable(tf.zeros([1,30]))
-# y1 = tf.nn.sigmoid(tf.matmul(x,W)+b1)
+    #1.创建模型 x,y,w,b,_y
+    x = tf.placeholder(tf.float32,[None,784],name = 'x')
+    w = tf.Variable(tf.zeros([784,10]))
+    b = tf.Variable(tf.zeros([10]))
+    y = tf.matmul(x,w)+b
+    y_ = tf.placeholder(tf.float32, [None,10])
 
-#直接使用softmax回归模型分类：
-y = tf.nn.softmax(tf.matmul(x,W)+b)
+    #2. 构建损失函数， 这里的交叉熵其实是log损失函数，是交叉熵在softmax上的推广
+    sy = tf.nn.softmax(y)
+    cross_entropy = tf.reduce_mean(-tf.reduce_mean(y_ * tf.log(sy),reduction_indices=[1]))
+    #防止数值计算问题，tf提供了整合的方法
+    cross_entropy1 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits = y))
+    #优化方法
+    train_step = tf.train.GradientDescentOptimizer(learning_rate = FLAGS.lrate).minimize(cross_entropy)
 
-#使用交叉熵损失函数
-#y_是一个新的占位符,占模型的输出结果   n*10
-y_ = tf.placeholder('float',[None,10])
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-#优化算法
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+    #3. 执行op
+    #使用InteractiveSession, 用x.run()代替 sess.run(x)
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
 
-####~~~评估模型
-#tf.arg_max()返回最大值索引（标签），cast将bool型结果转换成浮点型，然后计算均值得到准确率
-prediction = tf.equal(tf.arg_max(y_,1),tf.arg_max(y,1))
-accurary = tf.reduce_mean(tf.cast(prediction,"float"))
+    #Train
+    for _ in range(1000):
+        #定义好的next_batch方法
+        batch_xs, batch_ys = mnist.train.next_batch(FLAGS.batchsize)
+        sess.run(train_step, feed_dict = {x:batch_xs, y_: batch_ys})
+
+    # Evaluation
+    #计算准确率每一次迭代的准确率,使用测试集
+    #感觉tutorial有错误，此处应该用softmax的结果，而不是直接用y
+    correct_prediction = tf.equal(tf.argmax(tf.nn.softmax(y),1), tf.argmax(y_,1))
+    accurary = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print(sess.run(accurary, feed_dict = {x: mnist.test.images, y_: mnist.test.labels}))
+
+    sess.close()
+
+#运行程序
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', type = str, default = './MNIST_data', help = 'Directory for storing input minst data')
+    parser.add_argument('--lrate', type = float, default = 0.5, help = 'Learning rate')
+    parser.add_argument('--batchsize', type=int, default=100, help='mini_batch size')
+    FLAGS = parser.parse_args()
 
 
-#####~~~~开始训练
-#添加一个op初始化变量
-init_op = tf.initialize_all_variables()
-#在Session里面启动模型，并初始化变量
-with tf.Session() as sess:
-    sess.run(init_op)
-    #迭代训练
-    for i in xrange(1000):
-        #随机从数据集中取数据
-        batch_xs, batch_ys = mnist.train.next_batch(1000)
-        sess.run(train_step,feed_dict={x:batch_xs,y_:batch_ys})
-        if (i%10 == 0):
-            print sess.run(accurary, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+    tf.app.run(main = main, argv= None)
 
-'''~~~问题：
-#1. 为什么 单个softmax模型比逃一层sigmoid效果好：
-ans: 许多超参数没有训练
-#2. 为什么bantch内样本多了，学习效果反而下降(梯度下降学习率的实现问题)
-#3. batch和epoch
-'''
+
+
 
 
 
