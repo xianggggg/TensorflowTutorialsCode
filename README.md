a2ededb341267e4029d04552317a1e7746771897
rbz <zrb915@live.com>
2017-05-28 16:15:46 -0500

    05282017

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## Official tensorflow tutorial
 - tutorial_1: MNIST For ML Beginners
-- tutorial_2: 
+- tutorial_2: Build a Multilayer Convolutional Network
 
 ## Third-part tutorials
 - tutorial_Hinkhor:  http://www.kdnuggets.com/2016/08/gentlest-introduction-tensorflow-part-1.html
---
+++ official start/tutorial_2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#官方教程第二节，实现一个小的卷及神经网络
+#Robin  2016.6
+# =============================================================
+
+#引用
+#版本兼容
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+#解析命令行参数
+import argparse
+import sys
+#TF
+import tensorflow as tf
+import tensorflow.examples.tutorials.mnist.input_data as input_data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Relu RU
+## 梯度消失
